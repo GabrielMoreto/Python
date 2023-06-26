462a5d4f60bcf0e54b0fd94b99165cdc0343082a
GabrielMoreto <gabrielmozelli4825@gmail.com>
2023-06-26 16:00:02 -0300

    Códigos da linguagem Python

---
+++ exercicio.py
@@ -0,0 +1,20 @@
+# subst = "Python"
+# verbo = "é"
+# adjetivo = "fantástico"
+# print(subst, verbo, adjetivo, sep="_", end="!\n")
+
+# Para representar uma data, temos as variáveis dia, mes e ano:
+
+dia = 15
+mes = 10
+ano = 2015
+
+# Sem alterar as variáveis e sem passar nenhuma string adicional 
+# à função print(), como faríamos para ter como resultado da impressão, uma data formatada:
+
+# 15/10/2015
+
+dia = 15
+mes = 10
+ano = 2015
+print(dia, mes, ano, sep="/")
---
+++ tipagem_de_variaveis.py
@@ -0,0 +1,22 @@
+# Em Python temos os tipos de variáveis como por exemplo, string, inteiro, float e etc.
+# Segue alguns exemplos abaixo:
+
+# String:
+
+# Em Python para que uma variável seja considerada uma string basta que você a passe entre "".
+
+pais = "Brasil"
+
+# Para validar o tipo da variável basta executar a função type().
+
+print(type(pais))
+
+# Int:
+
+quantidade = 10
+print(type(quantidade))
+
+# Float:
+
+preco = 2.99
+print(type(preco))
