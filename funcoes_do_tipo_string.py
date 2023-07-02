ef839ba02f8ca3b74ef016e5b04d938c189c2a49
GabrielMoreto <gabrielmozelli4825@gmail.com>
2023-07-02 15:10:39 -0300

    Atualizando o arquivo funcoes_do_tipo_string.md

--- funcoes_do_tipo_string.py
+++ funcoes_do_tipo_string.py
@@ -1,2 +1,42 @@
-# Outras funções tem a função de alterar a palavra como upper, lower, capitalize ou strip. Quando falamos sobre esse tipo de funções, 
-# é importante lembrar que elas na verdade não alteram a string original. Por exemplo, veja esse código abaixo e tente descobrir a saída:
+# Função upper()
+
+# A função "upper()" serve para retornar a string com todas as letras em maiúsculo. Por exemplo:
+
+palavra = "python"
+palavra = palavra.upper()
+print(palavra)
+
+
+# Função lower()
+
+# A função "strip()" serve para retornar a string com todas as letras em minúsculo. Por exemplo:
+
+palavra = "python"
+palavra = palavra.upper()
+print(palavra)
+
+
+# Função capitalize()
+
+# A função "capitalize()" serve para retornar a string com a primeira letra em maiúsculo. Por exemplo:
+
+palavra = "python"
+palavra = palavra.capitalize()
+print(palavra)
+
+
+# Função strip()
+
+# A função "strip()" serve para remover todos os espaços em branco no início e no fim da string. Por exemplo:
+
+palavra = "   python   "
+palavra = palavra.strip()
+print(palavra)
+
+# OBSERVÇÃO
+
+# É importa lembrar que esse tipo de funções, na verdade não alteram a string original. Por exemplo, veja esse código abaixo:
+
+palavra = "python"
+palavra.upper()
+print(palavra) 
